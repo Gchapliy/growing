dbc6a3ecda7416f796508b41b8ebd0c072a4472d
Hryhorii Chaplii <hryhorii_chaplii@epam.com>
2020-01-10 17:29:06 +0200

    thoughts

--- sandbox/src/main/java/usingFeatures/Main.java
+++ sandbox/src/main/java/usingFeatures/Main.java
@@ -6,10 +6,6 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    public static List<String> list = Arrays.asList("item1", "item2", "item3", "item4", "item5");
     public static void main(String[] args) {
-        list = list.stream().filter(f -> !f.equalsIgnoreCase("item2")).collect(Collectors.toList());
-
-        System.out.println(list);
     }
 }
