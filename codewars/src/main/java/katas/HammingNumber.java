90a1894cb5655f174c5471eb0e52c56243cf3778
Hryhorii Chaplii <hryhorii_chaplii@epam.com>
2020-02-05 18:17:16 +0200

    codewars: katas: HammingNumber

--- codewars/src/main/java/katas/HammingNumber.java
+++ codewars/src/main/java/katas/HammingNumber.java
@@ -1,24 +1,26 @@
 package katas;
 
+import java.util.Arrays;
+
 /**
  * A *Hamming number* is a positive integer of the form 2i3j5k, for some non-negative integers i, j, and k.
- *
+ * <p>
  * Write a function that computes the nth smallest Hamming number.
- *
+ * <p>
  * Specifically:
- *
+ * <p>
  * The first smallest Hamming number is 1 = 203050
  * The second smallest Hamming number is 2 = 213050
  * The third smallest Hamming number is 3 = 203150
  * The fourth smallest Hamming number is 4 = 223050
  * The fifth smallest Hamming number is 5 = 203051
  * The 20 smallest Hamming numbers are given in example test fixture.
- *
+ * <p>
  * Your code should be able to compute all of the smallest 5,000 (Clojure: 2000) Hamming numbers without timing out.
  */
 public class HammingNumber {
     public static void main(String[] args) {
-        System.out.println(hamming(1));
+        /*System.out.println(hamming(1));
         System.out.println(hamming(2));
         System.out.println(hamming(3));
         System.out.println(hamming(4));
@@ -36,14 +38,38 @@ public class HammingNumber {
         System.out.println(hamming(16));
         System.out.println(hamming(17));
         System.out.println(hamming(18));
-        System.out.println(hamming(19));
+        System.out.println(hamming(19));*/
+        long strt = System.nanoTime();
+        for (int i = 1; i < 400; i++) {
+            System.out.println(hamming(i));
+        }
+        long fin = System.nanoTime();
+
+        System.out.println("time: " + (double)(fin - strt) / 1_000_000_000); //~0.5sec
     }
 
-    public static long hamming(int n){
-        int i = 0, j = 0, k = 0;
-        int a = 2, b = 3, c = 5;
+    public static long hamming(int n) {
+        int i = 0;
+        int j = 0;
 
+        System.out.print("for " + n + " is: ");
 
-        return 0;
+        while (j < n){
+            i++;
+
+            if(isHamming(i))j++;
+
+        }
+
+        return i;
+    }
+
+    public static boolean isHamming(int n) {
+        if (n == 1) return true;
+        if (n % 2 == 0) return isHamming(n / 2);
+        if (n % 3 == 0) return isHamming( n / 3);
+        if(n % 5 == 0) return isHamming( n / 5);
+
+        return false;
     }
 }
