be0c0004a242736cffd566ecd4dc6914583e72b2
Hryhorii Chaplii <hryhorii_chaplii@epam.com>
2020-02-06 18:47:52 +0200

    codewars: katas: HammingNumber

--- codewars/src/main/java/katas/HammingNumber.java
+++ codewars/src/main/java/katas/HammingNumber.java
@@ -19,77 +19,61 @@ import java.util.Arrays;
  * Your code should be able to compute all of the smallest 5,000 (Clojure: 2000) Hamming numbers without timing out.
  */
 public class HammingNumber {
+    private static boolean[] isRight = new boolean[2_000_000_000];
+    private static int pointer = 0;
+    private static int previousResult = 0;
+    private static int previousValue = 0;
+
     public static void main(String[] args) {
         long strt = System.nanoTime();
-        for (int i = 1; i < 1000; i++) {
+        for (int i = 1; i < 5000; i++) {
             System.out.println(hamming(i));
         }
         long fin = System.nanoTime();
 
-        System.out.println("time: " + (double)(fin - strt) / 1_000_000_000); //~0.5sec
+        System.out.println("time: " + (double) (fin - strt) / 1_000_000_000); //~7sec
     }
 
     public static long hamming(int n) {
-        int i = 0;
-        int j = 0;
+        int i = n > pointer ? 0 : previousResult;
+        int j = n > pointer ? 0 : previousValue;
+        previousValue = n;
 
         System.out.print("for " + n + " is: ");
-        int[] isHamm = new int[n * 2];
-        int[] notHamm = new int[n * 2];
 
-        int isHammPointer = 0, notHammPointer = 0;
-        while (j < n){
+        while (j < n) {
             i++;
 
-            if(isHamming(i, isHamm, notHamm, isHammPointer, notHammPointer)){
-                isHamm[isHammPointer] = i;
-                isHammPointer++;
-                j++;
-                if(isHamm.length - 1 < isHammPointer){
-                    isHamm = Arrays.copyOf(isHamm, isHamm.length * 2);
+            if (i <= pointer) {
+                if (isRight[i - 1]) {
+                    j++;
                 }
-            } else {
-                notHamm[notHammPointer] = i;
-                notHammPointer++;
-                if(notHamm.length - 1 < notHammPointer){
-                    notHamm = Arrays.copyOf(notHamm, notHamm.length * 2);
-                }
-            }
-            /*if(isHamming(i, isHamm, notHamm)){
-                isHamm[isHammPointer] = i;
-                isHammPointer++;
+                continue;
+            }else if(isHamming(i, i, isRight)) {
                 j++;
-            } else {
-                notHamm[notHammPointer] = i;
-                notHammPointer++;
+                isRight[pointer] = true;
             }
-*/
+
+            if (i > pointer) {
+                pointer++;
+                /*if (pointer > isRight.length - 1) {
+                    isRight = Arrays.copyOf(isRight, (int)(isRight.length *  1.2));
+                }*/
+            }
         }
 
+        previousResult = i;
         return i;
     }
 
-    public static boolean isHamming(int n, int[] notHamm) {
-        if(Arrays.binarySearch(notHamm, n) > -1) return false;
-        if (n == 1) return true;
-        if (n % 2 == 0) return isHamming(n / 2, notHamm);
-        if (n % 3 == 0) return isHamming( n / 3, notHamm);
-        if(n % 5 == 0) return isHamming( n / 5,  notHamm);
-
-        return false;
-    }
-
-    public static boolean isHamming(int n, int[] isHamm, int[] notHamm, int isP, int nP) {
+    public static boolean isHamming(int n, int target, boolean[] isRight) {
         if (n == 1) return true;
 
-        if(n < isHamm[isP - 1])
-            if(Arrays.binarySearch(isHamm, n) > -1) return true;
-        if(n < notHamm[nP - 1])
-            if(Arrays.binarySearch(notHamm, n) > -1) return false;
+        if (n < target) return isRight[n - 1];
 
-        if (n % 2 == 0) return isHamming(n / 2, isHamm, notHamm, isP, nP);
-        if (n % 3 == 0) return isHamming( n / 3, isHamm, notHamm, isP, nP);
-        if(n % 5 == 0) return isHamming( n / 5, isHamm, notHamm, isP, nP);
+        if (n % 2 == 0) return isHamming(n / 2, target, isRight);
+        if (n % 3 == 0) return isHamming(n / 3, target, isRight);
+        if (n % 5 == 0) return isHamming(n / 5, target, isRight);
 
         return false;
     }
