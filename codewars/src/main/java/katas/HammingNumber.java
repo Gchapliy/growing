60e5e89e6d4d1733f42f4ee6b0bc3e45ec654c5c
Hryhorii Chaplii <hryhorii_chaplii@epam.com>
2020-02-06 15:23:10 +0200

    codewars: katas: HammingNumber

--- codewars/src/main/java/katas/HammingNumber.java
+++ codewars/src/main/java/katas/HammingNumber.java
@@ -20,27 +20,8 @@ import java.util.Arrays;
  */
 public class HammingNumber {
     public static void main(String[] args) {
-        /*System.out.println(hamming(1));
-        System.out.println(hamming(2));
-        System.out.println(hamming(3));
-        System.out.println(hamming(4));
-        System.out.println(hamming(5));
-        System.out.println(hamming(6));
-        System.out.println(hamming(7));
-        System.out.println(hamming(8));
-        System.out.println(hamming(9));
-        System.out.println(hamming(10));
-        System.out.println(hamming(11));
-        System.out.println(hamming(12));
-        System.out.println(hamming(13));
-        System.out.println(hamming(14));
-        System.out.println(hamming(15));
-        System.out.println(hamming(16));
-        System.out.println(hamming(17));
-        System.out.println(hamming(18));
-        System.out.println(hamming(19));*/
         long strt = System.nanoTime();
-        for (int i = 1; i < 400; i++) {
+        for (int i = 1; i < 1000; i++) {
             System.out.println(hamming(i));
         }
         long fin = System.nanoTime();
@@ -53,22 +34,62 @@ public class HammingNumber {
         int j = 0;
 
         System.out.print("for " + n + " is: ");
+        int[] isHamm = new int[n * 2];
+        int[] notHamm = new int[n * 2];
 
+        int isHammPointer = 0, notHammPointer = 0;
         while (j < n){
             i++;
 
-            if(isHamming(i))j++;
-
+            if(isHamming(i, isHamm, notHamm, isHammPointer, notHammPointer)){
+                isHamm[isHammPointer] = i;
+                isHammPointer++;
+                j++;
+                if(isHamm.length - 1 < isHammPointer){
+                    isHamm = Arrays.copyOf(isHamm, isHamm.length * 2);
+                }
+            } else {
+                notHamm[notHammPointer] = i;
+                notHammPointer++;
+                if(notHamm.length - 1 < notHammPointer){
+                    notHamm = Arrays.copyOf(notHamm, notHamm.length * 2);
+                }
+            }
+            /*if(isHamming(i, isHamm, notHamm)){
+                isHamm[isHammPointer] = i;
+                isHammPointer++;
+                j++;
+            } else {
+                notHamm[notHammPointer] = i;
+                notHammPointer++;
+            }
+*/
         }
 
         return i;
     }
 
-    public static boolean isHamming(int n) {
+    public static boolean isHamming(int n, int[] notHamm) {
+        if(Arrays.binarySearch(notHamm, n) > -1) return false;
         if (n == 1) return true;
-        if (n % 2 == 0) return isHamming(n / 2);
-        if (n % 3 == 0) return isHamming( n / 3);
-        if(n % 5 == 0) return isHamming( n / 5);
+        if (n % 2 == 0) return isHamming(n / 2, notHamm);
+        if (n % 3 == 0) return isHamming( n / 3, notHamm);
+        if(n % 5 == 0) return isHamming( n / 5,  notHamm);
+
+        return false;
+    }
+
+    public static boolean isHamming(int n, int[] isHamm, int[] notHamm, int isP, int nP) {
+        if (n == 1) return true;
+
+        if(n < isHamm[isP - 1])
+            if(Arrays.binarySearch(isHamm, n) > -1) return true;
+        if(n < notHamm[nP - 1])
+            if(Arrays.binarySearch(notHamm, n) > -1) return false;
+
+        if (n % 2 == 0) return isHamming(n / 2, isHamm, notHamm, isP, nP);
+        if (n % 3 == 0) return isHamming( n / 3, isHamm, notHamm, isP, nP);
+        if(n % 5 == 0) return isHamming( n / 5, isHamm, notHamm, isP, nP);
 
         return false;
     }
