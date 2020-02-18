a277890b6091c21ae718d92b01d88aa339334335
Hryhorii Chaplii <hryhorii_chaplii@epam.com>
2020-02-18 18:21:13 +0200

    codewars: katas: MinimalSpanningTree

--- algorithms/src/main/java/someAlgorithms/MinimalSpanningTree.java
+++ algorithms/src/main/java/someAlgorithms/MinimalSpanningTree.java
@@ -1,11 +1,5 @@
 package someAlgorithms;
 
-import javax.jnlp.ClipboardService;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 /**
  * Kraskal algorithm
  */
@@ -31,42 +25,6 @@ public class MinimalSpanningTree {
         }
 
 
-        String[] graph = new String[edges.length];
-        Arrays.fill(graph, "");
-
-        graph[0] = edges[0];
-        int minWeight = weights[0];
-        int graphPosition = 1;
-
-        //Creating min span tree
-        for (int i = 1; i < edges.length; i++) {
-            String connectors = "";
-            boolean isCycle = false;
-            for (int j = 0; j < graphPosition; j++) {
-                if(edges[i].contains(graph[j].split("")[0])){
-                    if(connectors.contains(graph[j].split("")[1])){
-                        isCycle = true;
-                        break;
-                    }
-                    connectors += graph[j].split("")[0];
-                }
-                if(edges[i].contains(graph[j].split("")[1])){
-                    if(connectors.contains(graph[j].split("")[0])){
-                        isCycle = true;
-                        break;
-                    }
-                    connectors += graph[j].split("")[1];
-                }
-            }
-            if(!isCycle) {
-                graph[graphPosition] = edges[i];
-                minWeight += weights[i];
-                graphPosition++;
-            }
-        }
-
-        System.out.println(Arrays.toString(edges));
-        System.out.println(Arrays.toString(weights));
         return null;
     }
 }
