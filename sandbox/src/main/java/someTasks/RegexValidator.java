9216629819acdd434ce6b39f135495af3cf1f0b1
Hryhorii Chaplii <hryhorii_chaplii@epam.com>
2020-03-05 12:08:35 +0200

    sandbox: someTasks: RegexValidator sandbox: someTasks: xcelParser

--- sandbox/src/main/java/someTasks/RegexValidator.java
+++ sandbox/src/main/java/someTasks/RegexValidator.java
@@ -1,13 +1,16 @@
 package someTasks;
 
 public class RegexValidator {
-    private static final String REGEX = "[\\\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]{5,256}]";
+    private static final String REGEX_IS_LINK = "[\\\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]{5,256}]";
+    private static final String REGEX_ALL_LETTERS = "[a-zA-Zа-яА-Я]+";
 
     public static void main(String[] args) {
-        System.out.println(checkString("https://youtube.com"));
+
+        System.out.println(checkString("https://youtube.com", REGEX_IS_LINK));
+        System.out.println("|" + "Глагол ".replaceAll(REGEX_ALL_LETTERS, "") + "|");
     }
 
-    public static boolean checkString(String str){
-        return str.matches(REGEX);
+    public static boolean checkString(String str, String regex) {
+        return str.matches(regex);
     }
 }
