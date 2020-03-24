76705fe6131cb475a0c022ce06ded50ed09e7779
Hryhorii Chaplii <hryhorii_chaplii@epam.com>
2020-03-24 19:00:02 +0200

    sandbox: someTasks: xlsParser updated sandbox: someTasks: xlsxParser created

--- sandbox/src/main/java/usingFeatures/Main.java
+++ sandbox/src/main/java/usingFeatures/Main.java
@@ -1,5 +1,6 @@
 package usingFeatures;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -10,15 +11,8 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-        List<String> list = new LinkedList<>();
-        list.add("String1");
-        list.add("String2");
-        list.add("String2");
-        list.add("String3");
-        list.add("String4");
-        list.add("String4");
 
-        Set<String> set = new HashSet<>(list);
-        System.out.println(set);
     }
+
+
 }
