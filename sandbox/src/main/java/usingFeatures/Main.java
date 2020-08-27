0942cab90d582ba647fc07ea29f8cbfbd4ebf5b0
Gchapliy <grishachapliy1@gmail.com>
2020-08-27 10:04:22 +0300

    stream features

--- sandbox/src/main/java/usingFeatures/Main.java
+++ sandbox/src/main/java/usingFeatures/Main.java
@@ -16,7 +16,21 @@ public class Main {
 
         System.out.println(gson.toJson(new ArrayList<>(Arrays.asList("1", "2"))));*/
 
-        System.out.println(new Long(2).equals(new Long(2)));
-        System.out.println(new Long(1).equals(new Long(2)));
+//        System.out.println(new Long(2).equals(new Long(2)));
+//        System.out.println(new Long(1).equals(new Long(2)));
+
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        System.out.println(list);
+        List<Integer> list2 = Arrays.asList(2, 4, 7);
+        System.out.println(list2);
+
+        Integer[] arr = (Integer[]) list2.toArray();
+
+        System.out.println(Arrays.toString(arr));
+
+        //list.removeIf(p -> Arrays.binarySearch(arr, p) == -1);
+
+        list = list.stream().filter(f -> Arrays.binarySearch(arr, f) > -1).collect(Collectors.toList());
+        System.out.println("result -> " + list);
     }
 }
